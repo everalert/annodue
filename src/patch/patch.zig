22f5a3cfb3755bfdd62928fb030c2092fdbb8b83
everalert <everalert@gmail.com>
2024-01-29 21:31:41 +1100

    abstracted settings loader

--- src/patch/patch.zig
+++ src/patch/patch.zig
@@ -18,6 +18,9 @@ const ver_patch: u32 = 1;
 
 const mp = @import("patch_multiplayer.zig");
 const mem = @import("util/memory.zig");
+const settings = @import("util/settings.zig");
+const SettingsGroup = settings.SettingsGroup;
+const SettingsManager = settings.SettingsManager;
 const ini = @import("import/ini/ini.zig");
 
 const patch_size: u32 = 4 * 1024 * 1024; // 4MB
@@ -28,111 +31,57 @@ fn PtrMessage(alloc: std.mem.Allocator, ptr: usize, label: []const u8) void {
 }
 
 fn PatchDeathSpeed(min: f32, drop: f32) void {
-    //0x4C7BB8	4	DeathSpeedMin	float	325.0		SWEP1RCR.EXE+0x0C7BB8
     _ = mem.write(0x4C7BB8, f32, min);
-    //0x4C7BBC	4	DeathSpeedDrop	float	140.0		SWEP1RCR.EXE+0x0C7BBC
     _ = mem.write(0x4C7BBC, f32, drop);
 }
 
-const IniValue = union(enum) {
-    b: bool,
-    i: i32,
-    u: u32,
-    f: f32,
-};
-
-const IniValueError = error{
-    KeyNotFound,
-    NotParseable,
-    NotValid,
-};
-
-fn SetIniValue(set: *std.StringHashMap(IniValue), key: []const u8, value: []const u8) !void {
-    var kv = set.getEntry(key);
-    if (kv) |item| {
-        return switch (item.value_ptr.*) {
-            .b => {
-                if (std.mem.eql(u8, "true", value) or value[0] == '1') {
-                    item.value_ptr.*.b = true;
-                    return;
-                }
-                if (std.mem.eql(u8, "false", value) or value[0] == '0') {
-                    item.value_ptr.*.b = false;
-                    return;
-                }
-                return IniValueError.NotValid;
-            },
-            .i => {
-                item.value_ptr.*.i = try std.fmt.parseInt(i32, value, 10);
-            },
-            .u => {
-                item.value_ptr.*.u = try std.fmt.parseInt(u32, value, 10);
-            },
-            .f => {
-                item.value_ptr.*.f = try std.fmt.parseFloat(f32, value);
-            },
-        };
-    }
-}
-
 export fn Patch() void {
-    const memory = VirtualAlloc(null, patch_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE) catch unreachable;
+    const mem_alloc = MEM_COMMIT | MEM_RESERVE;
+    const mem_protect = PAGE_EXECUTE_READWRITE;
+    const memory = VirtualAlloc(null, patch_size, mem_alloc, mem_protect) catch unreachable;
     var off: usize = @intFromPtr(memory);
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
 
-    var gen_s = std.StringHashMap(IniValue).init(alloc);
-    defer gen_s.deinit();
-    gen_s.put("death_speed_mod_enable", .{ .b = false }) catch unreachable;
-    gen_s.put("death_speed_min", .{ .f = 325 }) catch unreachable;
-    gen_s.put("death_speed_drop", .{ .f = 140 }) catch unreachable;
+    // settings
 
-    var mp_s = std.StringHashMap(IniValue).init(alloc);
-    defer mp_s.deinit();
-    mp_s.put("multiplayer_mod_enable", .{ .b = false }) catch unreachable;
-    mp_s.put("patch_netplay", .{ .b = false }) catch unreachable;
-    mp_s.put("netplay_guid", .{ .b = false }) catch unreachable; // FIXME: causes crash on startup, other untested ones don't
-    mp_s.put("netplay_r100", .{ .b = false }) catch unreachable;
-    mp_s.put("patch_audio", .{ .b = false }) catch unreachable;
-    mp_s.put("patch_fonts", .{ .b = false }) catch unreachable; // working
-    mp_s.put("fonts_dump", .{ .b = false }) catch unreachable; // working?
-    mp_s.put("patch_tga_loader", .{ .b = false }) catch unreachable;
-    mp_s.put("patch_trigger_display", .{ .b = false }) catch unreachable; // working
-
-    var s = std.StringHashMap(*std.StringHashMap(IniValue)).init(alloc);
+    var s = SettingsManager.init(alloc);
     defer s.deinit();
-    s.put("general", &gen_s) catch unreachable;
-    s.put("multiplayer", &mp_s) catch unreachable;
 
-    // ini loading
+    var s_gen = SettingsGroup.init(alloc, "general");
+    defer s_gen.deinit();
+    s_gen.add("death_speed_mod_enable", bool, false);
+    s_gen.add("death_speed_min", f32, 325);
+    s_gen.add("death_speed_drop", f32, 140);
+    s.add(&s_gen);
 
-    const file = std.fs.cwd().openFile("annodue/settings.ini", .{}) catch unreachable;
-    defer file.close();
+    var s_mp = SettingsGroup.init(alloc, "multiplayer");
+    defer s_mp.deinit();
+    s_mp.add("multiplayer_mod_enable", bool, false);
+    s_mp.add("patch_netplay", bool, false);
+    s_mp.add("netplay_guid", bool, false); // FIXME: crash on startup, all others don't
+    s_mp.add("netplay_r100", bool, false);
+    s_mp.add("patch_audio", bool, false);
+    s_mp.add("patch_fonts", bool, false); // working
+    s_mp.add("fonts_dump", bool, false); // working?
+    s_mp.add("patch_tga_loader", bool, false);
+    s_mp.add("patch_trigger_display", bool, false); // working
+    s.add(&s_mp);
 
-    var parser = ini.parse(alloc, file.reader());
-    defer parser.deinit();
-
-    var set: *std.StringHashMap(IniValue) = undefined;
-    while (parser.next() catch unreachable) |record| {
-        switch (record) {
-            .section => |heading| set = s.getEntry(heading).?.value_ptr.*,
-            .property => |kv| SetIniValue(set, kv.key, kv.value) catch unreachable,
-            .enumeration => |value| _ = value,
-        }
-    }
+    s.read_ini(alloc, "annodue/settings.ini") catch unreachable;
 
     // random stuff
 
-    if (gen_s.get("death_speed_mod_enable").?.b) {
-        const dsm = gen_s.get("death_speed_min").?.f;
-        const dsd = gen_s.get("death_speed_drop").?.f;
+    if (s_gen.get("death_speed_mod_enable", bool)) {
+        const dsm = s_gen.get("death_speed_min", f32);
+        const dsd = s_gen.get("death_speed_drop", f32);
         PatchDeathSpeed(dsm, dsd);
     }
 
     // swe1r-patcher (multiplayer mod) stuff
 
-    if (mp_s.get("multiplayer_mod_enable").?.b) {
-        if (mp_s.get("fonts_dump").?.b) {
+    if (s_mp.get("multiplayer_mod_enable", bool)) {
+        if (s_mp.get("fonts_dump", bool)) {
             // This is a debug feature to dump the original font textures
             _ = mp.DumpTextureTable(alloc, 0x4BF91C, 3, 0, 64, 128, "font0");
             _ = mp.DumpTextureTable(alloc, 0x4BF7E4, 3, 0, 64, 128, "font1");
@@ -140,16 +89,16 @@ export fn Patch() void {
             _ = mp.DumpTextureTable(alloc, 0x4BF8B4, 3, 0, 64, 128, "font3");
             _ = mp.DumpTextureTable(alloc, 0x4BF984, 3, 0, 64, 128, "font4");
         }
-        if (mp_s.get("patch_fonts").?.b) {
+        if (s_mp.get("patch_fonts", bool)) {
             off = mp.PatchTextureTable(alloc, off, 0x4BF91C, 0x42D745, 0x42D753, 512, 1024, "font0");
             off = mp.PatchTextureTable(alloc, off, 0x4BF7E4, 0x42D786, 0x42D794, 512, 1024, "font1");
             off = mp.PatchTextureTable(alloc, off, 0x4BF84C, 0x42D7C7, 0x42D7D5, 512, 1024, "font2");
             off = mp.PatchTextureTable(alloc, off, 0x4BF8B4, 0x42D808, 0x42D816, 512, 1024, "font3");
             off = mp.PatchTextureTable(alloc, off, 0x4BF984, 0x42D849, 0x42D857, 512, 1024, "font4");
         }
-        if (mp_s.get("patch_netplay").?.b) {
-            const r100 = mp_s.get("netplay_r100").?.b;
-            const guid = mp_s.get("netplay_guid").?.b;
+        if (s_mp.get("patch_netplay", bool)) {
+            const r100 = s_mp.get("netplay_r100", bool);
+            const guid = s_mp.get("netplay_guid", bool);
             const traction: u8 = if (r100) 3 else 5;
             var upgrade_lv: [7]u8 = .{ traction, 5, 5, 5, 5, 5, 5 };
             var upgrade_hp: [7]u8 = .{ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
@@ -158,16 +107,16 @@ export fn Patch() void {
             off = mp.PatchNetworkUpgrades(off, upgrade_lv_ptr, upgrade_hp_ptr, guid);
             off = mp.PatchNetworkCollisions(off, guid);
         }
-        if (mp_s.get("patch_audio").?.b) {
+        if (s_mp.get("patch_audio", bool)) {
             const sample_rate: u32 = 22050 * 2;
             const bits_per_sample: u8 = 16;
             const stereo: bool = true;
             off = mp.PatchAudioStreamQuality(off, sample_rate, bits_per_sample, stereo);
         }
-        if (mp_s.get("patch_tga_loader").?.b) {
+        if (s_mp.get("patch_tga_loader", bool)) {
             off = mp.PatchSpriteLoaderToLoadTga(off);
         }
-        if (mp_s.get("patch_trigger_display").?.b) {
+        if (s_mp.get("patch_trigger_display", bool)) {
             off = mp.PatchTriggerDisplay(off);
         }
     }
---
+++ src/patch/util/settings.zig
@@ -0,0 +1,132 @@
+const settings = @This();
+
+const std = @import("std");
+const ini = @import("../import/ini/ini.zig");
+
+pub const IniValue = union(enum) {
+    b: bool,
+    i: i32,
+    u: u32,
+    f: f32,
+};
+
+pub const IniValueError = error{
+    KeyNotFound,
+    NotParseable,
+    NotValid,
+};
+
+pub const SettingsError = error{
+    ManagerNotInitialized,
+};
+
+pub const SettingsGroup = struct {
+    const Self = @This();
+    name: []const u8,
+    values: std.StringHashMap(IniValue),
+
+    pub fn init(alloc: std.mem.Allocator, name: []const u8) Self {
+        return .{
+            .values = std.StringHashMap(IniValue).init(alloc),
+            .name = name,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.values.deinit();
+    }
+
+    pub fn add(self: *Self, key: []const u8, comptime T: type, value: T) void {
+        switch (T) {
+            bool => self.values.put(key, .{ .b = value }) catch unreachable,
+            i32 => self.values.put(key, .{ .i = value }) catch unreachable,
+            u32 => self.values.put(key, .{ .u = value }) catch unreachable,
+            f32 => self.values.put(key, .{ .f = value }) catch unreachable,
+            else => return,
+        }
+    }
+
+    pub fn get(self: *Self, key: []const u8, comptime T: type) T {
+        var value = self.values.get(key);
+        return switch (T) {
+            bool => if (value) |v| v.b else false,
+            i32 => if (value) |v| v.i else 0,
+            u32 => if (value) |v| v.u else 0,
+            f32 => if (value) |v| v.f else 0,
+            else => undefined,
+        };
+    }
+
+    pub fn update(self: *Self, key: []const u8, value: []const u8) !void {
+        var kv = self.values.getEntry(key);
+        if (kv) |item| {
+            return switch (item.value_ptr.*) {
+                .b => {
+                    if (std.mem.eql(u8, "true", value) or value[0] == '1') {
+                        item.value_ptr.*.b = true;
+                        return;
+                    }
+                    if (std.mem.eql(u8, "false", value) or value[0] == '0') {
+                        item.value_ptr.*.b = false;
+                        return;
+                    }
+                    return IniValueError.NotValid;
+                },
+                .i => {
+                    item.value_ptr.*.i = try std.fmt.parseInt(i32, value, 10);
+                },
+                .u => {
+                    item.value_ptr.*.u = try std.fmt.parseInt(u32, value, 10);
+                },
+                .f => {
+                    item.value_ptr.*.f = try std.fmt.parseFloat(f32, value);
+                },
+            };
+        }
+    }
+};
+
+pub const SettingsManager = struct {
+    const Self = @This();
+    global: SettingsGroup,
+    groups: std.StringHashMap(*SettingsGroup),
+
+    pub fn init(alloc: std.mem.Allocator) Self {
+        return .{
+            .global = SettingsGroup.init(alloc, "__SettingsManager_global__"),
+            .groups = std.StringHashMap(*SettingsGroup).init(alloc),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.groups.deinit();
+        self.global.deinit();
+    }
+
+    pub fn add(self: *Self, group: *SettingsGroup) void {
+        self.groups.put(group.name, group) catch unreachable;
+    }
+
+    pub fn read_ini(self: *Self, alloc: std.mem.Allocator, filename: []const u8) !void {
+        const file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
+
+        var parser = ini.parse(alloc, file.reader());
+        defer parser.deinit();
+
+        var group: *SettingsGroup = &self.global;
+        while (try parser.next()) |record| {
+            switch (record) {
+                .section => |heading| {
+                    group = if (self.groups.getEntry(heading)) |g| g.value_ptr.* else &self.global;
+                },
+                .property => |kv| {
+                    try group.update(kv.key, kv.value);
+                },
+                .enumeration => |value| { // FIXME: implement
+                    _ = value;
+                },
+            }
+        }
+    }
+};

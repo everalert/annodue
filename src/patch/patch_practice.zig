f73caf659072c11ffec9c461b2ab5aa0c8a1b799
everalert <everalert@gmail.com>
2024-02-05 05:36:11 +1100

    tweak fps algorithm

--- src/patch/patch_practice.zig
+++ src/patch/patch_practice.zig
@@ -157,7 +157,7 @@ pub fn TextRender_Before(practice_mode: bool) void {
     state.was_in_race = in_race;
 
     const dt_f: f32 = mem.deref_read(&.{0xE22A50}, f32);
-    const fps_res: f32 = 10;
+    const fps_res: f32 = 100;
     state.fps = (state.fps * (fps_res - 1) + (1 / dt_f)) / fps_res;
 
     if (in_race) {

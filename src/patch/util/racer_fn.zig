dc39704fb2d8b943ec7e80509971fa26d4603fd1
everalert <everalert@gmail.com>
2024-02-16 18:16:11 +1100

    add some quad functions

--- src/patch/util/racer_fn.zig
+++ src/patch/util/racer_fn.zig
@@ -12,13 +12,19 @@ pub const swrText_CreateEntry2: *fn (x: u16, y: u16, r: u8, g: u8, b: u8, a: u8,
 
 pub const swrQuad_InitQuad: *fn (i: u16, spr: u32) callconv(.C) void = @ptrFromInt(0x4282F0);
 
-pub const swrQuad_LoadSpriteBlockIdx: *fn (i: u32) callconv(.C) u32 = @ptrFromInt(0x446FB0);
+pub const swrQuad_LoadSprite: *fn (i: u32) callconv(.C) u32 = @ptrFromInt(0x446FB0);
+
+pub const swrQuad_LoadTga: *fn (filename: [*:0]const u8, i: u32) callconv(.C) u32 = @ptrFromInt(0x4114D0);
 
 pub const swrQuad_SetActive: *fn (i: u16, on: u32) callconv(.C) void = @ptrFromInt(0x4285D0);
 
+pub const swrQuad_SetFlags: *fn (i: u16, flags: u32) callconv(.C) void = @ptrFromInt(0x4287E0);
+
+pub const swrQuad_ClearFlagsExcept: *fn (i: u16, flags: u32) callconv(.C) void = @ptrFromInt(0x4287E0);
+
 pub const swrQuad_SetPosition: *fn (i: u16, x: u16, y: u16) callconv(.C) void = @ptrFromInt(0x428660);
 
-pub const swrQuad_SetSize: *fn (i: u16, w: f32, h: f32) callconv(.C) void = @ptrFromInt(0x4286F0);
+pub const swrQuad_SetScale: *fn (i: u16, w: f32, h: f32) callconv(.C) void = @ptrFromInt(0x4286F0);
 
 pub const swrQuad_SetColor: *fn (i: u16, r: u8, g: u8, b: u8, a: u8) callconv(.C) void = @ptrFromInt(0x428740);
 

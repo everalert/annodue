b63ea2fbe7c95d75a0b1e9cebf26205d2ef17df3
everalert <everalert@gmail.com>
2024-02-24 00:25:13 +1100

    update test plugin

--- src/patch/dll_test.zig
+++ src/patch/dll_test.zig
@@ -3,6 +3,7 @@ const Self = @This();
 const std = @import("std");
 
 const GlobalState = @import("global.zig").GlobalState;
+const GlobalVTable = @import("global.zig").GlobalVTable;
 const COMPATIBILITY_VERSION = @import("global.zig").PLUGIN_VERSION;
 
 const r = @import("util/racer.zig");
@@ -17,51 +18,35 @@ export fn PluginName() callconv(.C) [*:0]const u8 {
 }
 
 export fn PluginVersion() callconv(.C) [*:0]const u8 {
-    return "0.0.0";
+    return "0.0.1";
 }
 
 export fn PluginCompatibilityVersion() callconv(.C) u32 {
     return COMPATIBILITY_VERSION;
 }
 
-export fn OnInit(gs: *GlobalState, initialized: bool) callconv(.C) void {
+export fn OnInit(gs: *GlobalState, gv: *GlobalVTable, initialized: bool) callconv(.C) void {
+    _ = gv;
     _ = initialized;
     _ = gs;
 }
 
-export fn OnInitLate(gs: *GlobalState, initialized: bool) callconv(.C) void {
+export fn OnInitLate(gs: *GlobalState, gv: *GlobalVTable, initialized: bool) callconv(.C) void {
+    _ = gv;
     _ = initialized;
     _ = gs;
 }
 
-export fn OnDeinit(gs: *GlobalState, initialized: bool) callconv(.C) void {
+export fn OnDeinit(gs: *GlobalState, gv: *GlobalVTable, initialized: bool) callconv(.C) void {
+    _ = gv;
     _ = initialized;
     _ = gs;
 }
-
-//export fn OnEnable(gs: *GlobalState, initialized: bool) callconv(.C) void {}
-//export fn OnDisable(gs: *GlobalState, initialized: bool) callconv(.C) void {}
 
 // HOOKS
 
-export fn MenuTrackSelectBefore(gs: *GlobalState, initialized: bool) callconv(.C) void {
-    var buf: [127:0]u8 = undefined;
-    _ = std.fmt.bufPrintZ(&buf, "~F0~1~sTest DLL MenuTrackSelect: state {x}, init {any}", .{
-        @intFromPtr(gs),
-        initialized,
-    }) catch unreachable;
-    rf.swrText_CreateEntry1(16, 16, 0, 0, 0, 190, &buf);
-}
-
-export fn TextRenderBefore(gs: *GlobalState, initialized: bool) callconv(.C) void {
-    var buf: [127:0]u8 = undefined;
-    _ = std.fmt.bufPrintZ(&buf, "~F0~1~sTest DLL TextRender: state {x}, init {any}", .{
-        @intFromPtr(gs),
-        initialized,
-    }) catch unreachable;
-    rf.swrText_CreateEntry1(16, 24, 0, 0, 0, 190, &buf);
-
-    if (gs.in_race.isOn()) {
-        rf.swrText_CreateEntry1(160, 120, 0, 0, 0, 190, "~F0~1~cTEST DLL TEXT TEXTRENDER_BEFORE");
-    }
+export fn EarlyEngineUpdateAfter(gs: *GlobalState, gv: *GlobalVTable, initialized: bool) callconv(.C) void {
+    _ = gv;
+    _ = initialized;
+    _ = gs;
 }

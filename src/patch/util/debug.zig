5e181a6aa6139fd62995df516be6868b7dfa3cf8
everalert <everalert@gmail.com>
2024-03-16 09:56:13 +1100

    fix: debug console not stealing focus, spawning in set location

--- src/patch/util/debug.zig
+++ src/patch/util/debug.zig
@@ -1,33 +1,41 @@
 const std = @import("std");
+
 const win = std.os.windows;
-const win32 = @import("zigwin32");
-const win32c = win32.system.console;
+const w32 = @import("zigwin32");
+const w32f = w32.foundation;
+const w32c = w32.system.console;
+const w32wm = w32.ui.windows_and_messaging;
 
 const global = @import("../global.zig");
+const mem = @import("memory.zig");
+const rc = @import("racer.zig").constants;
 
 const DebugConsole = struct {
     var initialized: bool = false;
-    var handle_out: win.HANDLE = undefined;
+    var handle_out: w32f.HANDLE = undefined;
+    var hwnd: ?w32f.HWND = null;
 };
 
 // NOTE: lazy loaded console alloc because comptime optimize mode checking was
 // removed in 0.11.0; consider changing to checking for debug build when it's back
 
-// FIXME: actual styling of the debug window
-// FIXME: also returning focus to the game on instantiation automatically
 fn Init() void {
     if (DebugConsole.initialized) return;
 
-    _ = win32c.AllocConsole();
-    DebugConsole.handle_out = win32c.GetStdHandle(.OUTPUT_HANDLE);
+    _ = w32c.AllocConsole();
+    DebugConsole.handle_out = w32c.GetStdHandle(.OUTPUT_HANDLE);
+    DebugConsole.hwnd = w32c.GetConsoleWindow();
     DebugConsole.initialized = true;
+
+    _ = w32wm.SetWindowPos(DebugConsole.hwnd, null, 0, 0, 640, 960, .{});
+    _ = w32wm.SetForegroundWindow(mem.read(rc.ADDR_HWND, w32f.HWND));
 }
 
 fn WriteConsole(handle: win.HANDLE, comptime fmt: []const u8, args: anytype) !void {
     const len = @as(usize, @truncate(std.fmt.count(fmt, args)));
     var buf: [1024]u8 = undefined;
     const out = try std.fmt.bufPrint(&buf, fmt, args);
-    _ = win32c.WriteConsoleA(handle, @ptrCast(&out[0]), len, null, null);
+    _ = w32c.WriteConsoleA(handle, @ptrCast(&out[0]), len, null, null);
 }
 
 pub fn ConsoleOut(comptime fmt: []const u8, args: anytype) !void {

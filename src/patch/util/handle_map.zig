7da3e079d83f38dc02dd98403c34685abe7285cc
Nathaniel <everalert@users.noreply.github.com>
2024-07-04 00:33:52 +1000

    feat: handle_map ownership

--- src/patch/util/handle_map.zig
+++ src/patch/util/handle_map.zig
@@ -10,6 +10,7 @@ pub fn Handle(comptime T: type) type {
     std.debug.assert(@typeInfo(T).Int.bits % 8 == 0);
 
     return extern struct {
+        owner: T = 0,
         generation: T = 0,
         index: T = 0,
     };
@@ -21,6 +22,7 @@ pub fn SparseIndex(comptime T: type) type {
     std.debug.assert(@typeInfo(T).Int.bits % 8 == 0);
 
     return extern struct {
+        owner: T = 0,
         generation: T = 0,
         index_or_next: T = 0,
     };
@@ -59,7 +61,8 @@ pub fn HandleMap(comptime T: type, comptime I: type) type {
 
         pub fn hasHandle(self: *Self, h: Handle(I)) bool {
             if (h.index < @as(I, @intCast(self.sparse_indices.items.len))) {
-                return self.sparse_indices.items[h.index].generation == h.generation;
+                return self.sparse_indices.items[h.index].generation == h.generation and
+                    self.sparse_indices.items[h.index].owner == h.owner;
             }
             return false;
         }
@@ -67,21 +70,23 @@ pub fn HandleMap(comptime T: type, comptime I: type) type {
         pub fn get(self: *Self, h: Handle(I)) ?*T {
             if (h.index < @as(I, @intCast(self.sparse_indices.items.len))) {
                 const entry = self.sparse_indices.items[h.index];
-                if (entry.generation == h.generation) {
+                if (entry.generation == h.generation and entry.owner == h.owner) {
                     return &self.values.items[entry.index_or_next];
                 }
             }
             return null;
         }
 
-        pub fn insert(self: *Self, value: T) !Handle(I) {
+        pub fn insert(self: *Self, owner: I, value: T) !Handle(I) {
             var handle: Handle(I) = undefined;
             if (self.next < @as(I, @intCast(self.sparse_indices.items.len))) {
                 var entry = &self.sparse_indices.items[self.next];
                 std.debug.assert(entry.generation < std.math.maxInt(I)); // "Generation sparse indices overflow"
 
+                entry.owner = owner;
                 entry.generation += 1;
                 handle = .{
+                    .owner = entry.owner,
                     .generation = entry.generation,
                     .index = self.next,
                 };
@@ -93,9 +98,11 @@ pub fn HandleMap(comptime T: type, comptime I: type) type {
                 std.debug.assert(self.next < std.math.maxInt(I)); // "Index sparse indices overflow"
 
                 handle = Handle(I){
+                    .owner = owner,
                     .index = @as(I, @intCast(self.sparse_indices.items.len)),
                 };
                 try self.sparse_indices.append(.{
+                    .owner = owner,
                     .index_or_next = @as(I, @intCast(self.handles.items.len)),
                 });
                 try self.handles.append(handle);
@@ -125,21 +132,43 @@ pub fn HandleMap(comptime T: type, comptime I: type) type {
             }
             return null;
         }
+
+        pub fn removeOwner(self: *Self, o: I) void {
+            const n = self.handles.items.len;
+            for (0..n) |i| {
+                const h = self.handles.items[n - i - 1];
+                if (h.owner == o)
+                    _ = self.remove(h);
+            }
+        }
     };
 }
 
 test {
-    var m = HandleMap(f32, u32).init(std.testing.allocator);
+    const HM = HandleMap(f32, u16);
+    const H = Handle(u16);
+
+    var m = HM.init(std.testing.allocator);
     defer m.deinit();
 
     var value: f32 = 123.456;
 
-    var h = try m.insert(value);
-    try std.testing.expect(m.hasHandle(h));
+    var h1: H = try m.insert(123, value);
+    var h2: H = try m.insert(456, value);
+    var h_: H = h1;
+    h_.owner = 789;
 
-    var ptr = m.get(h);
+    try std.testing.expect(m.hasHandle(h1));
+    try std.testing.expect(m.hasHandle(h2));
+    try std.testing.expect(!m.hasHandle(h_));
+
+    m.removeOwner(456);
+    try std.testing.expect(!m.hasHandle(h2));
+
+    var ptr = m.get(h1);
+    try std.testing.expect(ptr != null);
     try std.testing.expect(ptr.?.* == value);
 
-    _ = m.remove(h);
-    try std.testing.expect(!m.hasHandle(h));
+    _ = m.remove(h1);
+    try std.testing.expect(!m.hasHandle(h1));
 }

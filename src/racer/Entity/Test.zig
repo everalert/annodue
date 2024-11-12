461a14344d7cee0510ad2f1c38bf06a954e293d0
Nathaniel <everalert@users.noreply.github.com>
2024-11-12 16:20:23 +1100

    feat: add Test flags typedefs to racerlib

--- src/racer/Entity/Test.zig
+++ src/racer/Entity/Test.zig
@@ -38,13 +38,14 @@ pub const PLAYER_SLICE: **[SIZE]u8 = @ptrFromInt(PLAYER_PTR_ADDR); // TODO: conv
 
 // TODO: testing to assert entity size
 // TODO: finish filling in this
+// TODO: renaming to proper style
 pub const Test = extern struct {
     entity_magic: u32,
     entity_flags: u32,
     spawn: extern struct { position: [3]f32, orientation: [3]f32 }, // TODO: typedef, Location
     transform: Mat4x4,
-    flags1: u32, // TODO: enum
-    flags2: u32, // TODO: enum
+    flags1: TEST_FLAGS1,
+    flags2: TEST_FLAGS2,
     _unk_0068_006B: [4]u8,
     stats: TestStats,
     _unk_00AC_00DB: [0x30]u8, // TODO: typedef, LapCompStruct
@@ -155,101 +156,77 @@ pub const Test = extern struct {
     _unk_1510_1F27: [SIZE - 0x1510]u8,
 };
 
-// TODO: enum
-//0x0060 (u32)  Flags
-//	@0..3	'racing status'? all cleared when countdown ends, except @01 (see fn_474D80 "Go!!")
-//	1<<00  in countdown?
-//	1<<01	in race (racing), stays on while driving;  turned on when countdown ends (see fn_474D80 "Go!!")
-//			ref in 46CF00, checked to see if on while 00,02,03 are all off
-//	1<<02  probably completion status related
-//	1<<03  probably completion status related
-//	1<<04  ??? ref in 470610; braking related
-//			set to 1 when accelerating
-//			will only go back to 0 when pressing brake at very low speed
-//			will not go to 0 even at a standstill until you press brake
-//			nor if you press brake before going slow enough (doesn't buffer)
-//			only other time observed is when you die until you start accelerating again
-//	1<<05  race not ended?
-//			set to 1 in prerace media cutscene, stays on throughout race until crossing line
-//			referenced in 46CE30, 46CF00, 47B520, 479E10
-//	1<<06  ??? referenced in 47FDD0_LapCompletion, 46CF00_CalcTargetTurnRate, 470610
-//	1<<07  in autopilot
-//	1<<08  ??? referenced in 46B670, 46D170; related to going backward timer 0x110
-//	1<<09  is braking
-//	1<<10  is repairing;  set on during postrace
-//	1<<11  'reset pod' from LP;  ref in 46BEC0, Jdge_UiDrawRace_462B20; next to isdead
-//	1<<12  'respawn pod' from LP
-//			turns on when in autopilot, race not finished and deathspeed triggered (lmao)
-//			refs in 46BEC0, 4611F0, 46D170, 40B150, 47B520, 47B000_DeathSpeedHandler
-//			if you turn this on you die
-//	1<<13  has respawn invincibility
-//	1<<14  is dead
-//	1<<15  ??? - referenced in 46B670, seems race completion-related
-//	1<<16  ??? - referenced in 46B670, seems race completion-related
-//	1<<17  'TP pod to next spline point?' from LP;  ref in 46D170
-//	1<<18  ??? ref in 47B520
-//	1<<19  camera engine view
-//	1<<20  camera looking back
-//	1<<21  is able to charge boost
-//	1<<22  ???  called from 4783E0_ApplyAcceleration(), 470610
-//			locks thrust to 1.2 (from LP)
-//	1<<23  is boosting
-//	1<<24  ???  is going uphill? sudden pitch change flag? ??? ref in 4774F0; gravity related
-//	1<<25  in zon state;  not just on zon terrain, but presumably triggered by hitting zon terrain
-//	1<<26  in 'zoff' state;  not just on zoff terrain
-//			causes a hover state that automatically returns to 0 rotation with no input
-//			stays on for the duration of the extended hover you sometimes get leaving tubes
-//			presumably set by touching zoff terrain, but conditions not confirmed
-//	1<<27  is stopped or stopping?
-//			set on when at low but not necessarily 0 speed
-//			similar to 1<<04 but the speed threshold seems a little higher
-//	1<<28  ??? tested in speed function (from LP)
-//	1<<29
-//	1<<30  ??? was boosting last frame
-//	1<<31  ??? ref in 46BEC0, 41D930, 46D170, 477AD0; related to tilting, flags2 1<<27
-//0x0064 (u32)  Flags
-//	1<<00  on swamp terrain
-//	1<<01  ??? ref in 46D170
-//	1<<02  is not accelerating
-//	1<<03  is sliding? - confirmed directly switched on when airborne; also turns on with button?
-//	1<<04  ???  related to sliding as a check unsure what difference is
-//	1<<05  on side terrain;  seems to make AI manual tilt, also ref in 4783E0_ApplyAcceleration()
-//	1<<06  on mirr terrain
-//	1<<07  ??? elevation flag
-//			BWR tunnel/ice field/under some arches
-//			seems to activate when entering the loading area, not specific spots
-//			AMR pre-canyon area
-//			BEC 1st shortcut
-//			SR/ABY start and end bridges
-//			apparently tested in deathspeed function (from LP)
-//	1<<08
-//	1<<09  is airborne
-//	1<<10  tilt disabled, aka "magnet mode"
-//	1<<11  Boost Start Window;  ref in 45E200
-//	1<<12  boost start cancel (from LP)
-//	1<<13  boost start (from LP);  ref in 40B5E0
-//	1<<14  is exploding
-//	1<<15  is exploding;  left spinout (from LP),  ref in 46E150
-//			rel to flags1 1<<05 (race not ended) and flags2 1<<26 ('zoff state')
-//	1<<16  is exploding;  right spinout (from LP),  ref in 46E150
-//			rel to flags1 1<<05 (race not ended) and flags2 1<<26 ('zoff state')
-//	1<<17
-//	1<<18  on lava terrain
-//	1<<19  on fall terrain
-//	1<<20  on soft terrain;  e.g. BC beach
-//	1<<21  ??? written during track subsection load (see EXE+51B17)
-//	1<<22  ??? ref in 479E10; set or unset based on 1<<23, 'condition for flat' from LP
-//	1<<23  on flat terrain (from LP);  ref in 470610, 479E10
-//	1<<24  ??? set on at start of race BTC
-//	1<<25  race complete;  used for UI (from LP)
-//	1<<26  ??? called from 46CF00, 470610, 47B520; related to turn rate and autopilot
-//	1<<27  grounded? (from LP)
-//			called from 4783E0_ApplyAccel, 41D930, 470610
-//	1<<28  immunity? (from LP)
-//			called from 4783E0_ApplyAccel, 46D170, 470610, 47B520, 47B000_DeathSpeed
-//	1<<29  ??? ref in 47AB40, 47B520, 479E10, 47B000_DeathSpeedHandler
-//	1<<30
-//	1<<31  ??? called from 4783E0_ApplyAcceleration()
+// TODO: testing assert size 32 bits
+pub const TEST_FLAGS1 = packed struct {
+    IN_COUNTDOWN: bool, // 0..3 may be 4-bit int for race state
+    IN_RACE: bool,
+    _02: bool,
+    _03: bool,
+    IS_MOVING: bool,
+    RACE_NOT_ENDED: bool,
+    _06: bool,
+    IN_AUTOPILOT: bool,
+    _08: bool,
+    IS_BRAKING: bool,
+    IS_REPAIRING: bool,
+    QUEUE_RESET: bool,
+    QUEUE_RESPAWN: bool,
+    IS_RESPAWN_INVINCIBLE: bool,
+    IS_DEAD: bool,
+    _15: bool,
+    _16: bool,
+    _17: bool,
+    _18: bool,
+    CAMERA_ENGINE: bool,
+    CAMERA_LOOKBACK: bool,
+    BOOST_CAN_CHARGE: bool,
+    _22: bool,
+    IS_BOOSTING: bool,
+    _24: bool,
+    IN_ZON: bool,
+    IN_ZOFF: bool,
+    CAN_STOP: bool,
+    _28: bool,
+    _29: bool,
+    WAS_BOOSTING: bool, // on prev frame
+    _31: bool,
+};
+
+// TODO: testing assert size 32 bits
+pub const TEST_FLAGS2 = packed struct {
+    ON_SWMP_TERRAIN: bool,
+    _01: bool,
+    IS_NOT_ACCELERATING: bool,
+    IS_SLIDING: bool,
+    _04: bool,
+    ON_SIDE_TERRAIN: bool,
+    ON_MIRR_TERRAIN: bool,
+    _07: bool,
+    _08: bool,
+    IS_AIRBORNE: bool,
+    TILT_DISABLED: bool, // 'magnet mode'; TODO: confirm if terrain related and rename accordingly
+    CAN_BOOST_START: bool,
+    BOOST_START_CANCELLED: bool,
+    BOOST_START_ACTIVATED: bool,
+    IS_EXPLODING: bool,
+    IS_EXPLODING_RIGHT_SPIN: bool, // TODO: confirm if @14 is always on when @15 or @16 are
+    IS_EXPLODING_LEFT_SPIN: bool,
+    _17: bool,
+    ON_LAVA_TERRAIN: bool,
+    ON_FALL_TERRAIN: bool,
+    ON_SOFT_TERRAIN: bool,
+    _21: bool,
+    _22: bool,
+    ON_FLAT_TERRAIN: bool,
+    _24: bool,
+    RACE_COMPLETE: bool,
+    _26: bool,
+    _27: bool,
+    _28: bool,
+    _29: bool,
+    _30: bool,
+    _31: bool,
+};
 
 // HELPERS
 

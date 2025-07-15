87d920353c5742207620c51a673a8054e02e241a
Nathaniel <everalert@users.noreply.github.com>
2025-07-15 23:30:08 +1000

    feat: add decompressing png data

--- src/patch/util/png.zig
+++ src/patch/util/png.zig
@@ -1,6 +1,7 @@
 const PNG = @This();
 
 const std = @import("std");
+const zlib = std.compress.zlib;
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const LinearFifo = std.fifo.LinearFifo;
@@ -68,22 +69,22 @@ ColorType: ColorType,
 pub fn Read(alloc: Allocator, reader: anytype) !void {
     const CT = Chunk(@TypeOf(reader));
 
-    var data_buf: ArrayList(u8) = undefined;
-    var data_buf_set = false;
-    defer if (data_buf_set) data_buf.deinit();
-    const data_buf_w = data_buf.writer();
-
     // FIXME: this would work better in zig 0.13, because zlib decompress is a reader-writer
     // CT.Reader -> FIFO -> zlib decompressor -> decompressed output
     // - renew fifo writer each IDAT block
     // - main point of this is to decouple the IDAT block from the zlib
     //   decompressor so that multiple chunks can be used on it
+    var data_buf: ArrayList(u8) = undefined;
+    var data_buf_initialized = false;
+    defer if (data_buf_initialized) data_buf.deinit();
+    const data_buf_w = data_buf.writer();
     var data_fifo_buf: []u8 = try alloc.alloc(u8, 256);
     defer alloc.free(data_fifo_buf);
     var data_fifo = LinearFifo(u8, .Slice).init(data_fifo_buf);
     defer data_fifo.deinit();
-
-    //var data_zlib = std.compress.zlib.DecompressStream(CT.Reader);
+    var data_zlib_initialized = false;
+    var data_zlib: zlib.DecompressStream(@TypeOf(data_fifo.reader())) = undefined;
+    defer if (data_zlib_initialized) data_zlib.deinit();
 
     const signature: [8]u8 = sig: {
         var signature: [8]u8 = undefined;
@@ -92,16 +93,20 @@ pub fn Read(alloc: Allocator, reader: anytype) !void {
     };
     if (!std.mem.eql(u8, &signature, &Signature)) return error.SignatureInvalid;
 
-    var IHDR_width: u32 = 0;
-    var IHDR_height: u32 = 0;
-    var IHDR_bit_depth: u8 = 0;
-    var IHDR_color_type: u8 = 0;
+    var IHDR_width: u32 = 0xFFFFFFFF;
+    var IHDR_height: u32 = 0xFFFFFFFF;
+    var IHDR_bit_depth: u8 = 0xFF;
+    var IHDR_color_type: u8 = 0xFF;
+    var IHDR_compression_method: u8 = 0xFF;
+    var IHDR_filter_method: u8 = 0xFF;
+    var IHDR_interlace_method: u8 = 0xFF;
     var data_scanline_bytes: u32 = 0;
     var data_image_bytes: u32 = 0;
 
     var prev_chunk_type: u32 = 0;
     var IHDR_seen = false;
     var IDAT_seen = false;
+    var IDAT_done = false;
     var IEND_seen = false;
     while (true) {
         var chunk: CT = undefined;
@@ -122,15 +127,24 @@ pub fn Read(alloc: Allocator, reader: anytype) !void {
                     IHDR_height = try chunk_r.readIntBig(u32); // Height
                     IHDR_bit_depth = try chunk_r.readIntBig(u8); // BitDepth
                     IHDR_color_type = try chunk_r.readIntBig(u8); // ColorType
-                    _ = try chunk_r.readIntBig(u8); // CompressionMethod
-                    _ = try chunk_r.readIntBig(u8); // FilterMethod
-                    _ = try chunk_r.readIntBig(u8); // InterlaceMethod
+                    IHDR_compression_method = try chunk_r.readIntBig(u8); // CompressionMethod
+                    IHDR_filter_method = try chunk_r.readIntBig(u8); // FilterMethod
+                    IHDR_interlace_method = try chunk_r.readIntBig(u8); // InterlaceMethod
+
+                    if (IHDR_color_type != 0)
+                        return error.UnsupportedColorType;
+                    if (IHDR_compression_method != 0)
+                        return error.UnsupportedCompressionMethod;
+                    if (IHDR_filter_method != 0)
+                        return error.UnsupportedFilterMethod;
+                    if (IHDR_interlace_method != 0)
+                        return error.UnsupportedInterlaceMethod;
 
                     // filter type (1b) + bits needed for pixels in row, padded to next byte
                     data_scanline_bytes = (IHDR_width * IHDR_bit_depth + 7) / 8 + 1;
                     data_image_bytes = data_scanline_bytes * IHDR_height;
                     data_buf = try ArrayList(u8).initCapacity(alloc, data_image_bytes);
-                    data_buf_set = true;
+                    data_buf_initialized = true;
                 },
                 .IDAT => {
                     defer IDAT_seen = true;
@@ -138,16 +152,31 @@ pub fn Read(alloc: Allocator, reader: anytype) !void {
                         return error.IDATChunkNotConsecutive;
 
                     while (chunk.length_remaining > 0) {
+                        if (IDAT_done) {
+                            try chunk.ExhaustData();
+                            break;
+                        }
                         const count_w = try chunk_r.read(data_fifo.writableSlice(0));
                         data_fifo.update(count_w);
 
-                        // FIXME: for testing, switch to zlib decompressor
-                        const count_r = try data_buf_w.write(data_fifo.readableSlice(0));
-                        data_fifo.discard(count_r);
+                        while (data_fifo.readableLength() >= 4) {
+                            if (!data_zlib_initialized) {
+                                data_zlib_initialized = true;
+                                data_zlib = try zlib.decompressStream(alloc, data_fifo.reader());
+                            }
+                            const data_zlib_r = data_zlib.reader();
 
-                        // FIXME: only do this if end of zlib decompression
-                        // reached, and therefore no more need to process data
-                        if (false) try chunk.ExhaustData();
+                            if (data_buf.items.len == data_image_bytes) {
+                                _ = data_zlib_r.readByte() catch |e|
+                                    if (e != error.EndOfStream) return e;
+                                IDAT_done = true;
+                                try chunk.ExhaustData();
+                                break;
+                            }
+
+                            const value = try data_zlib_r.readByte();
+                            try data_buf_w.writeByte(value);
+                        }
                     }
                 },
                 .IEND => {
@@ -155,7 +184,6 @@ pub fn Read(alloc: Allocator, reader: anytype) !void {
                     if (chunk.length_remaining != 0)
                         return error.IENDChunkInvalidLength;
                 },
-                //else => chunk.ExhaustData(),
             }
         } else try chunk.ExhaustData();
 
@@ -185,25 +213,6 @@ pub fn Read(alloc: Allocator, reader: anytype) !void {
 // should be decoded as concatenated data, not as separate self-contained deflate
 // units; i.e. the main purpose of splitting IDATs is to reduce memory load on
 // encoders/decoders by limiting how big the read buffer needs to be to parse piecemeal
-
-// https://evanhahn.com/worlds-smallest-png/
-const TestImage = [_]u8{
-    0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, // signature
-    0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, // IHDR
-    0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
-    0x01, 0x00, 0x00, 0x00, 0x00, 0x37, 0x6E, 0xF9,
-    0x24,
-    0x00, 0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, // IDAT
-    0x78, 0x01, 0x63, 0x60, 0x00, 0x00, 0x00, 0x02,
-    0x00, 0x01, 0x73, 0x75, 0x01, 0x18,
-    0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, // IEND
-    0xAE, 0x42, 0x60, 0x82,
-};
-
-test "read png" {
-    var fbs = std.io.fixedBufferStream(&TestImage);
-    try Read(std.testing.allocator, fbs.reader());
-}
 
 // UTIL
 
@@ -325,111 +334,27 @@ pub fn Chunk(comptime ReaderType: type) type {
     };
 }
 
-// CRC
-// FIXME: probably just remove this after doing chunk writer; already integrated
-// into chunk reader
-
-// https://www.w3.org/TR/png-3/#D-CRCAppendix
-
-const CRCLUT: [256]u32 = crc: {
-    @setEvalBranchQuota(256 * 8 * 2);
-    var table: [256]u32 = undefined;
-    for (0..256) |n| {
-        var c: u32 = n;
-        for (0..8) |_| c = if ((c ^ 1) > 0) 0xEDB88320 ^ (c >> 1) else c >> 1;
-        table[n] = c;
-    }
-    break :crc table;
-};
-
-pub fn UpdateCRC(crc: u32, buf: []const u8) u32 {
-    var c = crc;
-    for (buf) |b| c = CRCLUT[(c ^ b) & 0xFF] ^ (c >> 8);
-    return c;
-}
-
-pub fn DoCRC(buf: []const u8) u32 {
-    return UpdateCRC(0xFFFFFFFF, buf);
-}
-
-/// a writer that calculates the PNG CRC of the bytes written through it
-pub fn CRCWriter(comptime WriterType: type) type {
-    return struct {
-        crc: u32,
-        child_stream: WriterType,
-
-        pub const Error = WriterType.Error;
-        pub const Writer = std.io.Writer(*Self, Error, write);
-
-        const Self = @This();
-
-        pub fn write(self: *Self, bytes: []const u8) Error!usize {
-            const amt = try self.child_stream.write(bytes);
-            self.crc = UpdateCRC(self.crc, bytes[0..amt]);
-            return amt;
-        }
-
-        pub fn writer(self: *Self) Writer {
-            return .{ .context = self };
-        }
-    };
-}
-
-/// a reader that calculates the PNG CRC of the bytes read through it
-pub fn CRCReader(comptime ReaderType: type) type {
-    return struct {
-        crc: u32,
-        child_stream: ReaderType,
-
-        pub const Error = ReaderType.Error;
-        pub const Reader = std.io.Reader(*Self, Error, read);
-
-        const Self = @This();
-
-        pub fn read(self: *Self, buf: []u8) Error!usize {
-            const amt = try self.child_stream.read(buf);
-            self.crc = UpdateCRC(self.crc, buf[0..amt]);
-            return amt;
-        }
-
-        pub fn reader(self: *Self) Reader {
-            return .{ .context = self };
-        }
-    };
-}
-
-pub fn crcWriter(child_stream: anytype) CRCWriter(@TypeOf(child_stream)) {
-    return .{ .crc = 0xFFFFFFFF, .child_stream = child_stream };
-}
-
-pub fn crcReader(child_stream: anytype) CRCReader(@TypeOf(child_stream)) {
-    return .{ .crc = 0xFFFFFFFF, .child_stream = child_stream };
-}
-
-// FIXME: finish test impl
-//test "CRCWriter" {
-//    var crc_stream = crcWriter(std.io.null_writer);
-//    const stream = crc_stream.writer();
-//
-//    const bytes = "yay" ** 100;
-//    stream.writeAll(bytes) catch unreachable;
-//    try std.testing.expect(crc_stream.crc == 0xFFFFFFFF); // FIXME: figure out actual crc manually
-//}
-
-// FIXME: finish test impl
-//test "CRCReader" {
-//    const bytes = "yay" ** 100;
-//    var fbs = std.io.fixedBufferStream(bytes);
-//    var crc_stream = crcReader(fbs.reader());
-//    const stream = crc_stream.reader();
-//
-//    //read and discard all bytes
-//    while (stream.readByte()) |_| {} else |err| {
-//        try std.testing.expect(err == error.EndOfStream);
-//    }
-//    try std.testing.expect(crc_stream.crc == 0xFFFFFFFF); // FIXME: figure out actual crc manually
-//}
-
-// helpers
+// HELPERS
 
 pub fn WriteGrey8(_: *PNG) void {}
+
+// TESTING
+
+// https://evanhahn.com/worlds-smallest-png/
+const TestImage = [_]u8{
+    0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, // signature
+    0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, // IHDR
+    0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+    0x01, 0x00, 0x00, 0x00, 0x00, 0x37, 0x6E, 0xF9,
+    0x24,
+    0x00, 0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, // IDAT
+    0x78, 0x01, 0x63, 0x60, 0x00, 0x00, 0x00, 0x02,
+    0x00, 0x01, 0x73, 0x75, 0x01, 0x18,
+    0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, // IEND
+    0xAE, 0x42, 0x60, 0x82,
+};
+
+test "read png" {
+    var fbs = std.io.fixedBufferStream(&TestImage);
+    try Read(std.testing.allocator, fbs.reader());
+}

9ffa07a65fb7d315f31888b83f9f29ee677d925e
Nathaniel <everalert@users.noreply.github.com>
2026-07-26 08:28:58 +1000

    fix(GAssetBuffer): texbuf_alloc undefined behaviour due to nullable type

--- src/patch/core/GAssetBuffer.zig
+++ src/patch/core/GAssetBuffer.zig
@@ -40,7 +40,7 @@ const GAssetBuffer = struct {
         0x62, 0xFE, 0xFF, 0x83, 0xC4, 0x04,
     };
     var texbuf_init_det: [32]u8 = undefined;
-    var texbuf_alloc: ?[]u32 = undefined;
+    var texbuf_alloc: []u32 = &.{};
 
     fn init() void {
         var d: x86.Detour = undefined;
@@ -57,16 +57,16 @@ const GAssetBuffer = struct {
     fn patch_texbuf() callconv(.C) void {
         const tex_count: u32 = @max(@max(s_texbuf_size, @as(*u32, @ptrFromInt(0xE9823C)).*), 1700);
         texbuf_alloc = coreAllocator().alloc(u32, tex_count) catch |err|
-            PPanic("patch_texbuf_init: allocation failed - {s}", .{@errorName(err)});
+            PPanic("patch_texbuf: alloc: {s}", .{@errorName(err)});
         //@memset(texbuf_alloc, 0);  // NOTE: seems to be unnecessary
 
         // patch TextureBuffer_LoadModelTexture (fn_447490)
-        _ = mem.write(0x4474B1, u32, @intFromPtr(texbuf_alloc.?.ptr));
-        _ = mem.write(0x4474C4, u32, @intFromPtr(texbuf_alloc.?.ptr));
-        _ = mem.write(0x447555, u32, @intFromPtr(texbuf_alloc.?.ptr));
+        _ = mem.write(0x4474B1, u32, @intFromPtr(texbuf_alloc.ptr));
+        _ = mem.write(0x4474C4, u32, @intFromPtr(texbuf_alloc.ptr));
+        _ = mem.write(0x447555, u32, @intFromPtr(texbuf_alloc.ptr));
         // patch TextureBuffer_ClearBufferAfterPtr (fn_4475D0)
-        _ = mem.write(0x4475D5, u32, @intFromPtr(texbuf_alloc.?.ptr));
-        _ = mem.write(0x4475E7, u32, @intFromPtr(texbuf_alloc.?.ptr + tex_count));
+        _ = mem.write(0x4475D5, u32, @intFromPtr(texbuf_alloc.ptr));
+        _ = mem.write(0x4475E7, u32, @intFromPtr(texbuf_alloc.ptr + tex_count));
     }
 
     fn settings_init(gf: *GlobalFn) void {
